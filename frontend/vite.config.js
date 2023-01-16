b351021b1d990d754bcaaa4d7f38bf0a11e95a79
comp <drobotyuk02@gmail.com>
2023-01-16 15:56:59 +0200

    fix entry point for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/main.jsx'),
+			name: 'A name?'
+			fileName: 'bookly-lib',
+		},
+		rollupOptions: {
+			external: ['react'],
+			output: {
+				globals: {
+					react: 'React',
+				}
+			}
+		}
+	},
   plugins: [react()],
 })
